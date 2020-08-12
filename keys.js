75d2f0f6a3cc375eda3cafa909a46907891cebd4
unknown <hamzabenlafkih90@gmail.com>
2020-08-12 17:44:58 +0100

    Node Api for squadChat

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI: "mongodb+srv://squadChat:DO2qyiGnuk6Krix3@cluster0.sb3s4.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_SECRET: "hamza99@react_js",
+}
---
+++ routes/auth.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../keys")
+const requireLogin = require("../middleware/requireLogin")
+
+router.post("/signup", (req, res) => {
+  const { name, email, password, pic } = req.body
+  if (!name || !email || !password) {
+    return res.status(422).json({ error: "pleaqe add all the fields" })
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "user already exist" })
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
+          pic
+        })
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "saved successfuly" })
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(422).json({ error: "please add email or password" })
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid email or password" })
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          //res.json({ message: "successfuly signed in" })
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET)
+          const { _id, name, email, followers, following, pic } = savedUser
+          res.json({ token, user: { _id, name, email, followers, following, pic } })
+        } else {
+          return res.status(422).json({ error: "Invalid email or password" })
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+})
+
+module.exports = router
